2f9207025ed4a7f05246b22699b2e4b26cc68ed1
Huynh Nguyen Khang <76195700+Krealvippro@users.noreply.github.com>
2022-09-10 22:21:07 +0700

    add item

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function Item() {
+export default function Item({info}) {
+  const {name, imgProduct, cost} = info
   return (
-      <div className='item'>
-
+    <div className="list-popular">       
+        <img className='imgProduct' src={imgProduct} alt="" />
+        <div className="name-cost">
+        <div className="costProduct">{cost}</div> 
+        <div className="name">
+          <a href=" ">{name}</a>
+        </div>
+        </div>
     </div>
   )
 }
