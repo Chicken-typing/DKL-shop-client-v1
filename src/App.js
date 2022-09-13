749fd731a2cf6f8976786a4bf55790965b2480b9
Huynh Nguyen Khang <76195700+Krealvippro@users.noreply.github.com>
2022-09-13 12:45:01 +0700

    ListItems, Item

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import "./styles/App.scss";
 import Register from "./pages/Register";
 import Footer from "./layout/Footer";
 import NewsItem from "./components/NewsItem";
+import Item from "./components/Item";
+import ListItem from "./containers/ListItem";
 function App() {
+  const data = [
+    {
+    id: 1,
+    name: "sneaker",
+    img: "./assets/images/adidas.png",
+    description: "the sneaker shoe",
+    cost:10,
+},
+{
+  id: 2,
+  name: "sneaker",
+  img: "./assets/images/nike.png",
+  description: "the sneaker nike shoe",
+  cost:20,
+}
+]
+  
   return (
     <div className="App">
-      <NewsItem />
+
+      <ListItem listItems={data}/>
     </div>
   );
 }
