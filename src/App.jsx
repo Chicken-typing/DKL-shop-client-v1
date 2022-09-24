f65c409c5bf18603fac2babd40694d0d9ba3c464
Huynh Nguyen Khang <76195700+Krealvippro@users.noreply.github.com>
2022-09-24 19:10:58 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import "./styles/App.scss";
 import Header from "./layout/Header";
-import ListNavigation from "./containers/ListNavigation";
-import MainPage from "./containers/MainPage";
-import Footer from "./layout/Footer";
-import Login from '../src/pages/Login'
 import Router from "./Router";
-import Woman from "./containers/Woman";
-
-
+import {useLocation} from 'react-router-dom';
 
 
 function App() {
-
+  const {pathname} = useLocation()
   
   
   return (
     <div className="App">
-      <Header/>  
-      {/* <ListNavigation/>       */}
-       <Router>
-          
+      <Header/>        
+       <Router> 
        </Router>  
-       {/* <Footer/> */}
-       {/* <Woman/> */}
     </div>
   );
 }
