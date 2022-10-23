aec0223324f91ce7ab14635b02416fb87076d7b0
Kreal244 <nuunkhang@gmail.com>
2022-10-23 19:28:59 +0700

    create saga get orders data

--- src/containers/AdminContainer/OrderTab/index.jsx
+++ src/containers/AdminContainer/OrderTab/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import { fetchOrders } from '../../../action'
 const OrderTab = () => {
+    const dispatch = useDispatch()
+    dispatch(fetchOrders())
+
     return (
         <div>
-            
+
         </div>
     );
 }
