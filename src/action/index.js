f95f7513ceae5e66435370b12a1f21dc2ca0d7fe
Kreal244 <nuunkhang@gmail.com>
2022-12-05 17:03:52 +0700

    update fetchUser

--- src/action/index.js
+++ src/action/index.js
@@ -2,29 +2,32 @@ import { fetchAPI, fetchAPISuccess, fetchAPIFailure } from "./fetch";
 import { fetchProduct, fetchProductSuccess } from "./fetchProducts";
 import { fetchOrders, fetchOrdersSuccess } from "./fetchOrders";
 import {
-    fetchStatic,
-    fetchAccessionSuccess,
-    fetchRevenueSuccess,
-    fetchNeededSuccess,
+  fetchStatic,
+  fetchAccessionSuccess,
+  fetchRevenueSuccess,
+  fetchNeededSuccess,
 } from "./fetchStatic";
 import { addToCart, removeFromCart, deleteFromCart } from "./Cart";
+import { fetchUser, fetchUserSuccess } from "./fetchUser";
 import { savePaymentMethod, saveShippingAddress } from "./Shipping";
 
 export {
-    fetchAPI,
-    fetchAPISuccess,
-    fetchAPIFailure,
-    fetchProduct,
-    fetchProductSuccess,
-    fetchStatic,
-    fetchAccessionSuccess,
-    fetchRevenueSuccess,
-    fetchNeededSuccess,
-    fetchOrders,
-    fetchOrdersSuccess,
-    addToCart,
-    removeFromCart,
-    deleteFromCart,
-    savePaymentMethod,
-    saveShippingAddress
+  fetchAPI,
+  fetchAPISuccess,
+  fetchAPIFailure,
+  fetchProduct,
+  fetchProductSuccess,
+  fetchStatic,
+  fetchAccessionSuccess,
+  fetchRevenueSuccess,
+  fetchNeededSuccess,
+  fetchOrders,
+  fetchOrdersSuccess,
+  addToCart,
+  removeFromCart,
+  deleteFromCart,
+  fetchUser,
+  fetchUserSuccess,
+  savePaymentMethod,
+  saveShippingAddress,
 };
