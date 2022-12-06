6552e8bd1c6e8ad8a8a6cc5300f52750d1b673b8
Lamvo27072001 <lampham27072001@gmail.com>
2022-12-06 18:29:15 +0700

    fix

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,22 +1,31 @@
 import { Modal, List, Avatar, Card, Typography, Popover, Form, Divider, Space, Tag } from 'antd';
 import { CheckCircleOutlined } from '@ant-design/icons'
-import Button from './Button'
 import React, { useState } from 'react';
+import confirmOrder from '../services/confirmProduct';
+import { useDispatch } from 'react-redux';
+import { fetchOrders } from '../action';
 const { Item } = List
 const OrderItem = props => {
     const { item } = props
+    const dispatch = useDispatch()
+    const Refetch = (callback) => {
+        return setTimeout(() => {
+            dispatch(callback);
+        }, 1000)
+    }
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isDone, setIsDone] = useState(false)
     const showModal = () => {
         setIsModalOpen(true);
     };
     const handleOk = () => {
         setIsModalOpen(false);
-        setIsDone(true)
+        confirmOrder(item.id, { isConfirm: true })
+        Refetch(fetchOrders())
     };
     const handleCancel = () => {
         setIsModalOpen(false);
-        setIsDone(false)
+        confirmOrder(item.id, { isConfirm: false })
+        Refetch(fetchOrders())
 
     };
     const content = (product) => (
@@ -36,7 +45,7 @@ const OrderItem = props => {
             </Form>
         </Card>)
     const done = (isDone) => (
-        isDone ? (<Tag icon={<CheckCircleOutlined />} color="success">
+        isDone ? (<Tag icon={<CheckCircleOutlined />} color="success" style={{ display: 'flex', alignItems: 'center' }}>
             DONE
         </Tag>) : ""
     )
@@ -47,7 +56,7 @@ const OrderItem = props => {
         <>
             <Item
                 onClick={showModal}
-                key={item.orderID}
+                key={item.id}
                 style={{
                     margin: 10,
                     width: 300
@@ -57,22 +66,24 @@ const OrderItem = props => {
                     display: 'flex',
                     justifyContent: 'space-between'
                 }}>
-                    {item.orderID}
-                    {done(isDone)}
+                    <Typography.Paragraph ellipsis>
+                        {item.id}
+                    </Typography.Paragraph>
+                    {done(item.isConfirm)}
                 </div>} hoverable>
                     <Item.Meta
                         title={<Typography.Paragraph ellipsis={true}>{item.userName}</Typography.Paragraph>}
                         avatar={<Avatar size='large' src={item.userImage} />}
                         description={<>
                             <Typography.Text keyboard>{item.totalPrice} VND</Typography.Text>
-                            {pay(item.paid)}
+                            {pay(item.isPaid)}
                         </>}
                     />
 
                 </Card>
             </Item>
             <Modal
-                title={"Order:" + item.orderID}
+                title={"Order:" + item.id}
                 centered
                 open={isModalOpen}
                 onOk={handleOk}
--- src/containers/AdminContainer/ProductListTab/index.jsx
+++ src/containers/AdminContainer/ProductListTab/index.jsx
@@ -1,30 +1,66 @@
-import { SearchOutlined } from '@ant-design/icons';
-import { Button, Input, Space, Table } from 'antd';
+import { DollarCircleOutlined, SearchOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Modal, Popconfirm, Space, Table, Tag } from 'antd';
 import React, { useRef, useState, useEffect } from 'react';
 import Highlighter from 'react-highlight-words';
 import './style.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProduct } from '../../../action';
-
+import deleteProduct from '../../../services/deleteProduct';
+import updateProduct from '../../../services/updateProduct';
+const { Item } = Form
 
 const ProductListTab = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchProduct())
     }, [])
-    const res = useSelector(state => state.fetchProduct.products)
+
+    const res = useSelector(state => state.fetchProduct.products.map((item) => {
+        return {
+            ...item,
+            key: item.id
+        }
+    }
+    ))
     const brands = [{
         text: "Nike",
-        value: "Nike"
+        value: "Nike",
+        key: 'nike'
     }, {
         text: "Adidas",
-        value: "Adidas"
+        value: "Adidas",
+        key: 'adidas'
     }
     ]
 
+    const categories =
+        [
+            {
+                key: 'man',
+                value: 'man',
+                text: 'Man'
+            },
+            {
+                key: 'woman',
+                value: 'woman',
+                text: 'Woman'
+            },
+            {
+                key: 'kid',
+                value: 'kid',
+                text: 'Kid'
+            },
+        ]
+    const Refetch = (callback) => {
+        return setTimeout(() => {
+            dispatch(callback);
+        }, 1000)
+    }
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [searchText, setSearchText] = useState('');
     const [searchedColumn, setSearchedColumn] = useState('');
+    const [editItem, setEditItem] = useState("")
     const searchInput = useRef(null);
     const handleSearch = (selectedKeys, confirm, dataIndex) => {
         confirm();
@@ -119,11 +155,18 @@ const ProductListTab = () => {
                 text
             ),
     });
+    const confirm = (index) => {
+        deleteProduct(index.id)
+
+        Refetch(fetchProduct())
+
+    };
     const columns = [
         {
             title: 'Name',
-            dataIndex: 'prodName',
-            ...getColumnSearchProps('prodName')
+            dataIndex: 'name',
+            ...getColumnSearchProps('name'),
+            width: 380
         },
         {
             title: 'Brand',
@@ -134,8 +177,28 @@ const ProductListTab = () => {
             onFilter: (value, record) => record.brand.indexOf(value) === 0
         },
         {
-            title: 'Type',
-            dataIndex: 'types',
+            title: 'Category',
+            dataIndex: 'category',
+            render: (_, { category }) =>
+            (
+                <>
+                    {category.map((tag) =>
+                        <Tag color={tag === "man"
+                            ? 'red' : tag === "woman"
+                                ? 'cyan'
+                                : 'green'}>
+                            {tag}
+                        </Tag>
+                    )
+                    }
+                </>
+            ),
+            filters: [
+                ...categories
+            ],
+            onFilter: (value, record) => record.category.includes(value)
+
+
         },
         {
             title: 'Price',
@@ -143,16 +206,113 @@ const ProductListTab = () => {
             defaultSortOrder: 'descend',
             sorter: (a, b) => a.price - b.price,
         },
+        {
+            width: 200,
+            render: (_, index) => (
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly'
+                }}>
+                    <Button type='ghost' onClick={() => {
+                        setIsModalOpen(true)
+                        setEditItem(index)
+                    }}>Edit</Button>
+                    <Popconfirm
+                        title="Are you sure?"
+                        onConfirm={() => { confirm(index) }}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <Button type='danger'>Delete</Button>
+                    </Popconfirm>
+                </div>
+            )
+        }
     ];
     const onChange = (pagination, filters, sorter, extra) => {
-        console.log('params', pagination, filters, sorter, extra);
+
+    };
+    const handleClose = () => {
+        setIsModalOpen(false);
+
+    }
+    const handleSubmit = (id, values) => {
+        updateProduct(id, values)
+        handleClose()
+
+        Refetch(fetchProduct())
+
     };
     return (
-        <Table columns={columns} dataSource={res} onChange={onChange} rowSelection pagination={{
-            pageSize: 9,
-            simple: true,
-        }} />
+        <>
+            <Table columns={columns} dataSource={res} onChange={onChange} pagination={{
+                pageSize: 8,
+                simple: true,
+            }} />
+            {EditForm(editItem, isModalOpen, handleSubmit, handleClose)}
+        </>
     );
 }
+const EditForm = (item, open, handleSubmit, handleClose) => {
+    const { id, name, brand, category, price } = item
+    if (item) {
+        return (
+            <Modal
+                open={open}
+                destroyOnClose
+                closable={false}
+                footer={null}
+                title={<>Edit: <Tag color='geekblue' style={{ marginLeft: 5 }}>{name}</Tag></>}>
+                <Form
+                    onFinish={(value) => handleSubmit(id, value)}
+                    labelCol={{
+                        span: 5,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                >
+                    <Item label="Brand">{brand}</Item>
+                    <Item label="Category">
+                        <Space direction='horizontal'>
+                            {category.map((tag) =>
+                                <Tag color={tag === "man"
+                                    ? 'red' : tag === "woman"
+                                        ? 'cyan'
+                                        : 'magnenta'}>
+                                    {tag}
+                                </Tag>)}
+                        </Space>
 
+                    </Item>
+                    <Item label="Price"
+                        name="price">
+                        <Input prefix={<DollarCircleOutlined />} defaultValue={price} />
+                    </Item>
+                    <Item label="Description"
+                        name="description" >
+                        <Input.TextArea
+                            rows={4}
+                            autoSize={
+                                {
+                                    minRows: 4,
+                                    maxRows: 4
+                                }
+                            }
+                            defaultValue={item.description} />
+                    </Item>
+                    <Item
+                        wrapperCol={{
+                            offset: 14,
+                            span: 16,
+                        }}>
+                        <Button type='ghost' style={{ margin: '0px 5px' }} onClick={handleClose}>Cancel</Button>
+                        <Button type="primary" htmlType="submit" style={{ margin: '0px 5px' }}>Submit</Button>
+                    </Item>
+                </Form>
+            </Modal>
+        )
+    }
+    else return (<></>)
+}
 export default ProductListTab;
