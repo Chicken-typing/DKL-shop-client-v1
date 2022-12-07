0bb4b28256303db888e7fbff393c0fad50f02a1d
Huynh Nguyen Khang <76195700+Kreal244@users.noreply.github.com>
2022-12-08 04:09:18 +0700

    update layout, fix login (#64)

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -87,9 +87,7 @@ function Header() {
             <Menu.Item>
               {({ active }) => (
                 <div
-                  href="#"
                   className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 cursor-pointer')}
-
                   onClick={() => {
                     dispatch(logout())
                     navigate('/')
