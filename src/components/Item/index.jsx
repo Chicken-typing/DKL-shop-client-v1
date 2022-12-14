7fdea21c1a0e83a219789868331612a3c18f8a3d
Lamvo27072001 <lampham27072001@gmail.com>
2022-12-14 17:57:17 +0700

    finish

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { addToCart } from '../../action';
 import './style.scss'
 import { DollarCircleOutlined } from '@ant-design/icons';
+import { Typography } from 'antd';
 
 export default function Item({ info }) {
 
 
   const { _id, name, defaultImage, brand, price } = info
-
+  const navigate = useNavigate()
   const dispatch = useDispatch();
 
 
@@ -23,8 +24,8 @@ export default function Item({ info }) {
       /></Link>
       <div className="name-cost">
         <div className="costProduct"><DollarCircleOutlined /> {price}</div>
-        <div className="name font-Helvetical">
-          <Link to={`/products/${info.name}`}>{name}</Link>
+        <div className="name font-Helvetical" onClick={() => navigate(`/products/${name}`)}>
+        <Typography.Paragraph ellipsis >{name}</Typography.Paragraph>
           <div className='type'>{brand}</div>
         </div>
       </div>
